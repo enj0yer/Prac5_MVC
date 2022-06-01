9746e92199c59307e88d5f6400b6cfa3acdeb6ba
enj0yer <90058431+enj0yer@users.noreply.github.com>
2022-06-01 20:37:42 +0400

    first commit

---
+++ Prac5_MVC/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+
+namespace Prac5_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static DataTable Table { get; } = new DataTable();
+
+        private static double Calculate(string Expression)
+        {
+            return Convert.ToDouble(Table.Compute(Expression, string.Empty));
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string CalcString)
+        {
+            bool error = false;
+            string result = "";
+
+            try
+            {
+                result = Calculate(CalcString).ToString();
+            }
+            catch
+            {
+                error = true;
+            }
+
+            if (error || result == "∞" || result == "-∞" || result == "не число")
+            {
+                result = "Ошибка в вычислениях";
+            }
+            ViewData["result"] = result;
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Prac5_MVC/Scripts/Scritps.js
@@ -0,0 +1,30 @@
+﻿
+document.querySelectorAll(".act_button").forEach(el =>
+    el.addEventListener("click", ev => {
+        if (document.getElementById("result").value === "Ошибка в вычислениях") {
+            document.getElementById("result").value = "";
+        }
+        document.getElementById("result").value += ev.target.value;
+        ev.preventDefault();
+    }));
+
+document.querySelectorAll(".clear_field").forEach(el =>
+    el.addEventListener("click", ev => {
+        document.getElementById("result").value = "";
+        ev.preventDefault();
+    }));
+
+document.querySelectorAll(".delete_last").forEach(el =>
+    el.addEventListener("click", ev => {
+        document.getElementById("result").value = document.getElementById("result").value.slice(0, -1);
+        ev.preventDefault();
+    }));
+
+document.onkeydown = (ev) => {
+    let keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+", "Enter", "/", "*", "Delete", "Backspace"];
+
+    if (keys.indexOf(ev.key) !== -1) {
+        document.getElementById(ev.key).click();
+    }
+    ev.preventDefault();
+};
